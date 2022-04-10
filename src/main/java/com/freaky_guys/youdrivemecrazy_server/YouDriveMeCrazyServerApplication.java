ec8ffcfff0afb72bceb8b58f89fb53b44b4de2aa
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-04-10 15:09:14 +0900

    first commit

---
+++ src/main/java/com/freaky_guys/youdrivemecrazy_server/YouDriveMeCrazyServerApplication.java
@@ -0,0 +1,13 @@
+package com.freaky_guys.youdrivemecrazy_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouDriveMeCrazyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouDriveMeCrazyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freaky_guys/youdrivemecrazy_server/YouDriveMeCrazyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freaky_guys.youdrivemecrazy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouDriveMeCrazyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
